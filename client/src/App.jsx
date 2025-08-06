5df2fba9e1d89fb3fa0ab7af20745e05b96b0a41
charles <charlesgintoki@gmail.com>
2025-08-06 14:51:03 +0800

    done on navbar and sidebar and responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Navbar from './components/navbar'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
