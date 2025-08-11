2f3eac9b9a9607d391d9fa3a535979b5a6d7b30d
charles <charlesgintoki@gmail.com>
2025-08-11 16:13:34 +0800

    done set up server, created db connection and cloudinary and created product and user models

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+//App Config
+const app = express();
+const port = process.env.PORT || 4000;
+
+//database connection
+connectDB();
+//cloudinary connection
+connectCloudinary();
+
+//Middlewares
+app.use(express.json())
+//access backend from any ip 
+app.use(cors())
+
+// api endpoint
+
+app.get('/', (req, res) => {
+  res.send("API Working")
+})
+
+
+app.listen(port, () => console.log("Server Started on Port: "+ port))
