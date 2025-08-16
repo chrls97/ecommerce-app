49c003650e3654290f51e3f0b623de3dc9cb3d0f
charles <charlesgintoki@gmail.com>
2025-08-16 15:59:38 +0800

    done on useroders client connected to userorders api

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -50,7 +50,16 @@ const allOrders = async (req,res) => {
 
 //Display all orders for user
 const userOrders = async (req,res) => {
-  
+  try{
+    const {userId} = req.body
+
+    const orders = await orderModel.find({userId})
+    res.json({success:true, orders})
+  }catch(error){
+    console.log(error);
+    res.json({success:false, message:error.message})
+    
+  }
 }
 
 //update order status
